de3cd710664ff945ae875f12502795327db47b39
Arathi <ssdfa7@gmail.com>
2017-03-06 17:00:29 +0800

    添加phpExcel库，添加了AdminLTE的登陆与注册模板
    Signed-off-by: Arathi <ssdfa7@gmail.com>

---
+++ resources/views/auth/login_ng.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>{{ config('app.name', 'Laravel') }}</title>
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="stylesheet" href="{{$felcdn_bootstrap}}/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="{{$felcdn_font_awesome}}/css/font-awesome.min.css" />
+  <link rel="stylesheet" href="{{$felcdn_ionicons}}/css/ionicons.min.css" />
+  <link rel="stylesheet" href="{{$felcdn_adminlte}}/css/AdminLTE.min.css" />
+  <link rel="stylesheet" href="{{$felcdn_icheck}}/skins/square/blue.css" />
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="{!! url('home') !!}"><b>La</b>shboard</a>
+  </div>
+  
+  <div class="login-box-body">
+    <p class="login-box-msg">用户登录</p>
+
+    <form action="{!! url('auth/login') !!}" method="post">
+      {!! csrf_field() !!}
+      <div class="form-group has-feedback">
+        <input type="email" name="email" value="{{ old('email') }}" class="form-control" placeholder="E-mail">
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" name="password" id="password" class="form-control" placeholder="请输入密码">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+            <label>
+              <input type="checkbox" name="remember"> 记住我
+            </label>
+          </div>
+        </div>
+        <!-- /.col -->
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">登录</button>
+        </div>
+        <!-- /.col -->
+      </div>
+    </form>
+
+    <a href="#">忘记密码？点此重置</a><br>
+    <a href="{!! url('auth/register') !!}" class="text-center">没有账号？点此注册</a>
+
+  </div>
+  <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+
+<script src="{{$felcdn_jquery}}/jquery.min.js"></script>
+<script src="{{$felcdn_bootstrap}}/js/bootstrap.min.js"></script>
+<script src="{{$felcdn_icheck}}/icheck.min.js"></script>
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' // optional
+    });
+  });
+</script>
+</body>
+</html>
---
+++ resources/views/auth/register_ng.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>{{ config('app.name', 'Laravel') }}</title>
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="stylesheet" href="{{$felcdn_bootstrap}}/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="{{$felcdn_font_awesome}}/css/font-awesome.min.css" />
+  <link rel="stylesheet" href="{{$felcdn_ionicons}}/css/ionicons.min.css" />
+  <link rel="stylesheet" href="{{$felcdn_adminlte}}/css/AdminLTE.min.css" />
+  <link rel="stylesheet" href="{{$felcdn_icheck}}/skins/square/blue.css" />
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body class="hold-transition register-page">
+<div class="register-box">
+  <div class="register-logo">
+    <a href="{!! url('home') !!}"><b>La</b>shboard</a>
+  </div>
+
+  <div class="register-box-body">
+    <p class="login-box-msg">注册新用户</p>
+
+    <form action="{!! url('auth/register') !!}" method="post">
+      {!! csrf_field() !!}
+      <div class="form-group has-feedback">
+        <input type="text" name="name" value="{{ old('name') }}" class="form-control" placeholder="用户名">
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="text" name="nickname" value="{{ old('nickname') }}" class="form-control" placeholder="昵称">
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="email" name="email" value="{{ old('email') }}" class="form-control" placeholder="E-mail">
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password"  name="password" class="form-control" placeholder="请输入密码">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" name="password_confirmation" class="form-control" placeholder="请确认密码">
+        <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+            <label>
+              <input type="checkbox"> 我同意<a href="#">用户协议</a>
+            </label>
+          </div>
+        </div>
+        <!-- /.col -->
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">注册</button>
+        </div>
+        <!-- /.col -->
+      </div>
+    </form>
+
+    <a href="{!! url('auth/login') !!}" class="text-center">已有账户？点此登陆</a>
+  </div>
+  <!-- /.form-box -->
+</div>
+<!-- /.register-box -->
+
+<script src="{{$felcdn_jquery}}/jquery.min.js"></script>
+<script src="{{$felcdn_bootstrap}}/js/bootstrap.min.js"></script>
+<script src="{{$felcdn_icheck}}/icheck.min.js"></script>
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' // optional
+    });
+  });
+</script>
+</body>
+</html>
