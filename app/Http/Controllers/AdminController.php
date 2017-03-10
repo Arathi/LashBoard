661ad128596fa07634301eed40b93ef8d6b5fea2
Arathi <ssdfa7@gmail.com>
2017-03-10 14:28:05 +0800

    添加了通过json方式配置菜单项
    Signed-off-by: Arathi <ssdfa7@gmail.com>

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -36,7 +36,70 @@ abstract class AdminController extends Controller
 
     protected function get_menu_tree()
     {
-        return config('menu.default-menu', []);
+        $menuConfigMode = config('menu.config_mode', 'json');
+        $menuTree = array();
+
+        if ($menuConfigMode == 'json')
+        {
+            $menuJsonPath = resource_path('assets/menus.json');  // TODO 暂时写死
+            $jsonContents = file_get_contents($menuJsonPath);
+            $menusJson = json_decode($jsonContents);
+            foreach ($menusJson as $menuJson)
+            {
+                // TODO 合并菜单项样式（header、listview、active等）
+                // TODO 检查当前页面是否需要设置为active样式
+                $menu_item_style = '';
+                $styles = $menuJson->styles;
+                foreach ($styles as $style)
+                {
+                    $menu_item_style .= $style . ' ';
+                }
+                $menu_item_style = trim($menu_item_style);
+
+                // 生成URL（根据target_type和target生成）
+                // target_type 有 url, route
+                $target = $menuJson->target;
+                $url = '#';
+                if ($menuJson->target_type == 'url')
+                {
+                    if ( strncmp('https://', $target, 7) == 0 || 
+                            strncmp('http://', $target, 6) == 0 || 
+                            strncmp('//', $target, 2) == 0) 
+                    {
+                        // url为"http://"、"https://"、"//" 开头时，为绝对地址
+                        $url = $target;
+                    }
+                    else
+                    {
+                        // 其他情况下使用url生成站内地址
+                        $url = url( $target );
+                    }
+                }
+                else if ($menuJson->target_type == 'route')
+                {
+                    // 如果使用route模式，请在route中命名路由，否则会报错
+                    $url = route( $target );
+                }
+
+                $caption = $menuJson->caption;
+
+                // TODO 根据$menuJson['icon']的值自动拼接
+                $icon_style = $menuJson->icon;
+
+                $menuTree[] = [
+                    'style' => $menu_item_style,
+                    'url' => $url,
+                    'caption' => $caption,
+                    'icon' => $icon_style
+                ];
+            }
+        }
+        else if ($menuTree == 'db')
+        {
+            //
+        }
+
+        return $menuTree;
     }
 
     protected function get_messages()
--- config/menu.php
+++ config/menu.php
@@ -1,46 +1,11 @@
 <?php
 return [
     // json, db
-    'config_mode' => 'db',
+    'config_mode' => 'json',
 
     // 如果从JSON文件中获取菜单，在此项中填写文件名
     'json_file' => '',
 
     // 如果从数据库中获取菜单，在此项中填写菜单相关的模型
     'menu_model' => App\Models\Menu::class,
-
-    /*
-    'default_menu' => [
-        'platform_mgmt' => [
-            'style' => 'header',
-            'caption' => '平台管理',
-            'icon' => '',
-            'url' => '#',
-        ],
-        'user_mgmt' => [
-            'style' => '',
-            'caption' => '用户管理',
-            'icon' => 'fa fa-user',
-            'url' => url('admin/user'),
-        ],
-        'role_mgmt' => [
-            'style' => '',
-            'caption' => '角色管理',
-            'icon' => 'fa fa-users',
-            'url' => url('admin/role'),
-        ],
-        'menu_mgmt' => [
-            'style' => '',
-            'caption' => '菜单管理',
-            'icon' => 'fa fa-list',
-            'url' => url('admin/menu'),
-        ],
-        'auth_mgmt' => [
-            'style' => '',
-            'caption' => '权限管理',
-            'icon' => 'fa fa-user-secret',
-            'url' => url('admin/auth'),
-        ]
-    ]
-    */
 ];
